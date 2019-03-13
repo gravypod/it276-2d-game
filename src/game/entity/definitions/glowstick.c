de2fffc06c8112f0acecde0fbb1d7c5b38bdc5da
Joshua Katz <gravypod@gravypod.com>
2019-03-13 12:34:50 -0400

    Move collection maintnence into it's own function set

--- src/game/entity/definitions/glowstick.c
+++ src/game/entity/definitions/glowstick.c
@@ -4,14 +4,13 @@
 int num_glowsticks = 0;
 entity_t *glowsticks[MAX_NUM_GLOWSTICKS];
 
-void entity_glowstick_init(entity_t *entity) {
-    entity->type = entity_type_glowstick;
-    entity->free = entity_glowstick_free;
-    entity->sprite = gf2d_sprite_load_image("images/kenny-nl/generic-items/genericItem_color_024.png");
+void entity_glowsticks_add(entity_t *entity)
+{
     glowsticks[num_glowsticks++] = entity;
 }
 
-void entity_glowstick_free(entity_t *entity) {
+void entity_glowsticks_remove(entity_t *entity)
+{
     int i;
     for (i = 0; i < entity_glowsticks_deployed(); i++) {
         if (entity->id == entity_glowsticks_get(i)->id) {
@@ -24,6 +23,18 @@ void entity_glowstick_free(entity_t *entity) {
     }
 
     glowsticks[MAX_NUM_GLOWSTICKS - 1] = NULL;
+}
+
+void entity_glowstick_init(entity_t *entity) {
+    entity->type = entity_type_glowstick;
+    entity->free = entity_glowstick_free;
+    entity->sprite = gf2d_sprite_load_image("images/kenny-nl/generic-items/genericItem_color_024.png");
+
+    entity_glowsticks_add(entity);
+}
+
+void entity_glowstick_free(entity_t *entity) {
+    entity_glowsticks_remove(entity);
 }
 
 int entity_glowsticks_deployed() {
