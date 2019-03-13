4d89e865b1273a3d30d8263eddf5f69adb2b8940
Joshua Katz <gravypod@gravypod.com>
2019-03-13 13:57:57 -0400

    Bugs track bags of chips over players

--- src/game/entity/definitions/bug.c
+++ src/game/entity/definitions/bug.c
@@ -1,12 +1,39 @@
 #include <game/entity/manager.h>
 #include "bug.h"
 #include "player.h"
+#include "plop.h"
 
 #define ENTITY_BUG_SPEED_NORMAL 1
 
 #define SPRITE_HEIGHT 128
 #define SPRITE_WIDTH 128
 
+entity_t *entity_bug_most_valuable_entity_find(entity_t *us)
+{
+    int num_bags_of_chips = entity_plop_bagofchips_count();
+
+    if (num_bags_of_chips == 0) {
+        return player;
+    }
+
+    entity_t *chips = entity_plop_bagofchips_get(0);
+
+    for (int i = 1; i < num_bags_of_chips; i++) {
+        entity_t *search = entity_plop_bagofchips_get(i);
+
+        Vector2D temp;
+        vector2d_sub(temp, us->position, search->position);
+        float search_distance = vector2d_magnitude(temp);
+        vector2d_sub(temp, us->position, chips->position);
+        float last_distance = vector2d_magnitude(temp);
+
+        if (last_distance > search_distance) {
+            chips = search;
+        }
+    }
+
+    return chips;
+}
 
 void entity_bug_init(entity_t *entity)
 {
@@ -50,7 +77,8 @@ void entity_bug_update(entity_t *entity)
     if (entity->health <= 0) {
         entity->speed = 0.0f;
     } else {
-        vector2d_sub(entity->velocity, player->position, entity->position);
+        entity_t *following = entity_bug_most_valuable_entity_find(entity);
+        vector2d_sub(entity->velocity, following->position, entity->position);
         vector2d_normalize(&entity->velocity);
     }
 }
