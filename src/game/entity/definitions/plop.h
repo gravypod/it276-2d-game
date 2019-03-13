5f207de9d2cb1c9070fb4723f9e1806c47864e09
Joshua Katz <gravypod@gravypod.com>
2019-03-13 13:40:40 -0400

    Add indexing functionality to plops

--- src/game/entity/definitions/plop.h
+++ src/game/entity/definitions/plop.h
@@ -5,4 +5,12 @@
 
 void entity_plop_init(entity_t *entity);
 
+int entity_plop_glowsticks_count();
+int entity_plop_bagofchips_count();
+int entity_plop_superglues_count();
+
+entity_t *entity_plop_glowsticks_get(int slot);
+entity_t *entity_plop_bagofchips_get(int slot);
+entity_t *entity_plop_superglues_get(int slot);
+
 #endif
