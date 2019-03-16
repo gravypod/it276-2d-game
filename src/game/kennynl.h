51b4135cc10600938f913736e81d7e9222ebc37a
Joshua D Katz <gravypod@gravypod.com>
2019-03-16 17:08:56 -0400

    [Game Effect] Tracking & Hatching Eggs
    When a player transitions stepon states we spawn an egg 20% of the time.
    This egg then randomly hatches to create a new bug.

--- src/game/kennynl.h
+++ src/game/kennynl.h
@@ -1,8 +1,11 @@
-//
-// Created by gravypod on 3/16/19.
-//
-
 #ifndef GF2D_KENNYNL_H
 #define GF2D_KENNYNL_H
 
-#endif //GF2D_KENNYNL_H
+#define KENNY_NL_IMAGE(number) \
+    "images/kenny-nl/generic-items/genericItem_color_" \
+    # number \
+    ".png"
+
+#define KENNY_NL_EGG KENNY_NL_IMAGE(127)
+
+#endif
